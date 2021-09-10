52d37565dff1f1c472dcaa0c1c1ac851720e84fb
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-09-10 10:28:27 -0400

    starting over using psuedocode

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,6 @@
-/**
- * When button is pressed: series of prompts for password criteria
- * When prompted for password criteria ---
- *    select which criteria to include in the password
- * When prompted for the length of the password ---
- *    choose a length of at least 8 characters and no more than 128 characters
- * When asked for character types to include in the password ---
- *     confirm whether or not to include lowercase, uppercase, numeric, 
- *     and/or special characters
- * When answer each prompt ---
- *     my input should be validated and at least one character type 
- *     should be selected
- * When all prompts are answered ---
- *     password is generated that matches the selected criteria
- * When the password is generated ---
- *     the password is either displayed in an alert or written to the page
- */
-
-
-//Ask what each section means, generateBtn, writePassword()
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+//looking at html for this file, in reference to html button
 
 // Write password to the #password input
 function writePassword() {
@@ -30,60 +11,50 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//when someone clicks button, run the writePassword function
 
-var generatePassword = function() {
+
+var generatePassword = function() {//returning the password
+  // return "superSecretPassword"; //prints in the textarea
   document.getElementById("generate").innerHTML=
-  window.alert("Welcome to the Password Generator!");
+  alert("Welcome to the Password Generator!");
   
-  // make this (put this within) a function??
-  //Issue ATM - putting in letters or other numbers besides 1 and 2 stops the code
-  var passwordChoice = 
-  window.prompt("Let's begin generating a password for you!\n\n Enter '1' to customize the LENGTH of the password OR enter '2' to customize the CHARACTERS in the password."
-  );
-  if (passwordChoice === "" || passwordChoice === null) {
-    window.alert("Invalid entry. Please try again.");
-    return generatePassword; //how to i make this only repeat window.prompt?
-  }
-  if (passwordChoice === "1") {
-    passwordLength();
-  }
+  var passwordLength = prompt("How many characters would you like in your password? Choose a length of at least 8 characters and no more than 128 characters.");
+  var passwordLength = parseInt(passwordLength);
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert("Your password is " + lengthQuestion + " characters long.");
+  } 
+
+  // if (!passwordLength) {
+  //   alert("Invalid entry. Try again.");
+  // }
+  //  if (!passwordLength) { //checking to see if it's false
 }
+
+
 /**
- * Issues ATM for passwordLength
- * When putting in a space, the prompt still continues
- * When putting in letters, the prompt still continues
+ * PSUEDOCODE
+ * user clicks on the generate password button, gets a greeting
+ * user clicks again
+ * window prompt asks how many characters for the password
+ * window mentions must be between 8 and 128 characters
+ * if the user puts anything else but what's between 8-128
+ *    window alert to try again, then loops back to ask the question
+ * user selects character between 8 and 128 characters
+ * window alert reads back how many characters the password will be
+ * user clicks again
+ * window asks if they'd like uppercase letters in password, yes or no
+ *    if yes, the do math random on variable that has all uppercase letters
+ *    if no, skip to next --
+ * window asks if they'd like lowercase letters in password, yes or no
+ *    if yes, the do math random on variable with all lowercase letters
+ *    if no, skip to next --
+ * window asks if they'd like numbers in their password
+ *    if yes, the do math random on variable with all numbers
+ *    if no skip to next --
+ * window asks if they'd like special characters in their password
+ *    if yes, the do math random on variable with all lowercase letters
+ * window alert then says password generated
+ * generatePassword then prints the generated password into the textarea
  */
-
-var passwordLength = function() {
-  var lengthQuestion = window.prompt("How many characters would you like in your password? Choose a length of at least 8 characters and no more than 128 characters.");
-  if (lengthQuestion === "" || lengthQuestion === null) {
-    window.alert("Invalid entry. Try again.");
-    return passwordLength();
-  }
-
-  if (lengthQuestion >= 8 && lengthQuestion <= 128) {
-    window.alert("Your password is " + lengthQuestion + " characters long.");
-  }
-  else {
-    window.alert("Invalid entry. Try again.");
-    return passwordLength();
-  }
-  //add code to go to the next section
-}
-
-//NEED HELP
-//function needs to go through the string of each selection based on the
-//number of characters the player picked in passwordLength
-//then print it into the password box
-
-// var passwordSelection = function() {
-//   var passwordComplete = "";
-//   var selectUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   var selectLowercase = "abcdefghijklmnopqrstuvwxyz";
-//   var selectNumbers = '0123456789';
-//   var selectSpecial = "!#$%&'()*+,-./:;<=>?@[^_`{|}~"";
-// for (var i = 0; writePassword??)
-//     text += selectCharacters.charAt(Math.floor(Math.random() * selectCharacters.length));
-//   return passwordComplete;
-// }
-// }
