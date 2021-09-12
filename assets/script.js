2f87f2624605d2aacc6e0226d44bbc44fd27e536
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-09-12 17:40:54 -0400

    screenshots and final code

--- assets/script.js
+++ assets/script.js
@@ -177,29 +177,3 @@ var getSpecial = function() {
     }
   }
 }
-
-/**
- * PSUEDOCODE
- * user clicks on the generate password button, gets a greeting
- * user clicks again
- * window prompt asks how many characters for the password
- * window mentions must be between 8 and 128 characters
- * if the user puts anything else but what's between 8-128
- *    window alert to try again, then loops back to ask the question
- * user selects character between 8 and 128 characters
- * window alert reads back how many characters the password will be
- * user clicks again
- * window asks if they'd like uppercase letters in password, yes or no
- *    if yes, the do math random on variable that has all uppercase letters
- *    if no, skip to next --
- * window asks if they'd like lowercase letters in password, yes or no
- *    if yes, the do math random on variable with all lowercase letters
- *    if no, skip to next --
- * window asks if they'd like numbers in their password
- *    if yes, the do math random on variable with all numbers
- *    if no skip to next --
- * window asks if they'd like special characters in their password
- *    if yes, the do math random on variable with all lowercase letters
- * window alert then says password generated
- * generatePassword then prints the generated password into the textarea
- */
